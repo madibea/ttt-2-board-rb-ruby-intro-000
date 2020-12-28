9bc28413722db349a247776bcff057fc6abc135f
Madison McLenaghan Dunn <madibea@gmail.com>
2020-12-28 21:00:57 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,3 @@
-board = [row 0, row 1, row 2]
-
 row = [" ", " ", " "]
 
 row [0]
@@ -7,3 +5,17 @@ row [0]
 row [1]
 
 row [2]
+
+row [3]
+
+row [4]
+
+row [5]
+
+row [6]
+
+row [7]
+
+row [8]
+
+row [9]
